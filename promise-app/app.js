62793ca3d15fa49a1ae29859e39f20c6b2b83bcb
Christopher Walsh <walshification@gmail.com>
2016-07-17 18:04:46 -0500

    simulates a async request to resolve a promise

---
+++ promise-app/app.js
@@ -0,0 +1,21 @@
+angular.module('app', [])
+.factory('getData', getData)
+.run(function(getData) {
+  var promise = getData()
+    .then(function(string){
+      console.log(string);
+    });
+});
+
+function getData($timeout, $q) {
+  return function() {
+    var defer = $q.defer()
+
+    // simulated async function
+    $timeout(function() {
+      defer.resolve('data received!');
+    }, 2000);
+
+    return defer.promise;
+  };
+};
