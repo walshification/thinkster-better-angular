c37934602e879ce47b6c3d640726de2830f0d755
Christopher Walsh <walshification@gmail.com>
2016-07-17 13:56:04 -0500

    completes controllers app with "controller as" syntax

--- controller-app/app.js
+++ controller-app/app.js
@@ -4,15 +4,28 @@ var app = angular.module(
 );
 
 // Register the controller using the controller function on modules.
-app.controller(
-  'MainCtrl',         // defines the name of the controller
-  function ($scope){  // the function that defines our controller's behavior
-    // Parameters passed to the controller function are the dependencies that are
-    // injected into it.
-    $scope.message = 'hello';
+// app.controller(
+//   'MainCtrl',         // defines the name of the controller
+//   function ($scope){  // the function that defines our controller's behavior
+//     // Parameters passed to the controller function are the dependencies that are
+//     // injected into it.
+//     $scope.message = 'hello';
+// 
+//     // functions can be created on the scope to manipulate data in the view
+//     $scope.updateMessage = function (message){
+//       $scope.message = message;
+//     };
+// });
 
-    // functions can be created on the scope to manipulate data in the view
-    $scope.updateMessage = function (message){
-      $scope.message = message;
-    };
+// Using "controller as" syntax:
+app.controller('MainCtrl', function (){  // $scope is no longer a dependency.
+  // Create a reference to this so we don't lose context of our controller when we
+  // create/call functions within our controller.
+  var self = this;
+
+  self.message = 'hello';
+
+  self.updateMessage = function(message){
+    self.message = message;
+  };
 });
