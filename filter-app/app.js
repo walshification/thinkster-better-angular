15e6f8aec61bf5297a8176e8871445175afad0a4
Christopher Walsh <walshification@gmail.com>
2016-07-18 20:30:51 -0500

    adds custom filter and explanatory comments

--- filter-app/app.js
+++ filter-app/app.js
@@ -3,18 +3,31 @@ var filterApp = angular.module('app', []);
 filterApp.controller('FilterCtrl', function(){
   var self = this;
 
+  // Initialize the controller with default information.
   self.people = [
     {
-      name: "Eric Simons",
+      name: "eric simons",
       born: "Chicago",
     },
     {
-      name: "Albert Pai",
+      name: "albert pai",
       born: "Taiwan",
     },
     {
-      name: "Matthew Greenster",
+      name: "matthew greenster",
       born: "Virginia",
     },
   ];
-});
+}).filter(  // filter method on the controller interface.
+  // Defines a filter 'capitalize' to invoke the filter function.
+  'capitalize',
+  // The actual filter function.
+  function(){
+    // This is the function Angular will execute when the expression is evaluated.
+    return function (text) {
+      // text is the original string output of the Angular expression.
+      return text.toUpperCase();
+      // and we simply return the text in uppercase.
+    };
+  }
+);
