c60f4b19a652c205f8712194bd096e91155f6b5e
Christopher Walsh <walshification@gmail.com>
2016-07-18 20:41:41 -0500

    clarifies filter method is on module

--- filter-app/app.js
+++ filter-app/app.js
@@ -1,5 +1,19 @@
 var filterApp = angular.module('app', []);
 
+filterApp.filter(  // filter method on the module interface.
+  // Defines a filter 'capitalize' to invoke the filter function.
+  'capitalize',
+  // The actual filter function.
+  function(){
+    // This is the function Angular will execute when the expression is evaluated.
+    return function (text) {
+      // text is the original string output of the Angular expression.
+      return text.toUpperCase();
+      // and we simply return the text in uppercase.
+    };
+  }
+);
+
 filterApp.controller('FilterCtrl', function(){
   var self = this;
 
@@ -18,16 +32,4 @@ filterApp.controller('FilterCtrl', function(){
       born: "Virginia",
     },
   ];
-}).filter(  // filter method on the controller interface.
-  // Defines a filter 'capitalize' to invoke the filter function.
-  'capitalize',
-  // The actual filter function.
-  function(){
-    // This is the function Angular will execute when the expression is evaluated.
-    return function (text) {
-      // text is the original string output of the Angular expression.
-      return text.toUpperCase();
-      // and we simply return the text in uppercase.
-    };
-  }
-);
+});
