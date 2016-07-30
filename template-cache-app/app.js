d3ca4841e1ab74f6f458fe46a550d1ee4bfde2ee
Christopher Walsh <walshification@gmail.com>
2016-07-30 18:37:45 -0500

    adds hardcoded landing page

--- template-cache-app/app.js
+++ template-cache-app/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/', {
+    controller: 'TestController as test',
+    template: 'Hello {{ test.user.name }}!',
+  })
+  .otherwise('/');
+});
+
+app.controller('TestController', function(){
+  var self = this;
+
+  self.user = { name: 'Chris' };
+});
