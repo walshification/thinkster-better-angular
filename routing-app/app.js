ae63638f9ad1fab9dc5ed8d6d95e85f5f8a48d91
Christopher Walsh <walshification@gmail.com>
2016-07-31 11:01:04 -0500

    adds angular router and simple template for ng-view

--- routing-app/app.js
+++ routing-app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('app', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when('/',
+    {
+      templateUrl: "app.html",
+      controller: "AppController",
+      controllerAs: "app",
+    }
+  );
+});
+
+app.controller("AppController", function(){
+  var self = this;
+  self.message = "The app routing is working!";
+});
